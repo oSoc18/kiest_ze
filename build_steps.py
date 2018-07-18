279b91f3983be9a2f8a5f6c020d23df94fe49303
Emile Sonneveld <contact@emilesonneveld.be>
2018-07-18 14:00:06 +0200

    moved yarn project to separate folder

---
+++ build_steps.py
@@ -0,0 +1,16 @@
+from shutil import copyfile
+from distutils.dir_util import copy_tree
+import os
+
+if not os.path.exists("kiestze_yarn/dist/static/css/"):
+    os.makedirs("kiestze_yarn/dist/static/css/")
+
+copyfile("kiestze_yarn/src/static/css/style.css", "kiestze_yarn/dist/static/css/style.css")
+copyfile("kiestze_yarn/src/static/css/bootstrap.min.css", "kiestze_yarn/dist/static/css/bootstrap.min.css")
+
+
+copy_tree("kiestze_yarn/dist/static/", "kiestze_django/static/")
+
+copyfile("kiestze_yarn/dist/detail.html", "kiestze_django/templates/detail.html")
+copyfile("kiestze_yarn/dist/index.html", "kiestze_django/templates/index.html")
+copyfile("kiestze_yarn/dist/lijst.html", "kiestze_django/templates/lijst.html")
