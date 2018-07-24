df38b3959dad4b2bee53b7de481ea8b9693e9282
Emile Sonneveld <contact@emilesonneveld.be>
2018-07-24 11:49:43 +0200

    Import Politieker airtable to edit sustem. (Using a bit of the database)

--- python_importer/airtables_postgresql.py
+++ python_importer/airtables_postgresql.py
@@ -1,64 +1,69 @@
 import requests
+import uuid  # https://github.com/skorokithakis/shortuuid
+import datetime
+import psycopg2
 
-
+# HACK: https://stackoverflow.com/questions/7505988/importing-from-a-relative-path-in-python/7506029#7506029
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'kiestze_django', 'kiestze_django'))
+import secret
+#import ..kiestze_django/kiestze_django/secret.py
+# C:\Users\emill\dev\kiest_ze\python_importer\airtables_postgresql.py
+# C:\Users\emill\dev\kiest_ze\kiestze_django\kiestze_django\secret.py
 
 # same as in python_importer
-sql_common = """
-
-USE kiestze
-
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
--- otherwise we get ERROR 2006 (HY000) at line 18: MySQL server has gone away
-SET global max_allowed_packet=64*1024*1024;
+sql = """
+INSERT INTO public.kiestze_user_edit(guid, column_name, accepted_date, suggested_value, politieker_id) VALUES
 
 """
-monster_query = sql_common
-
-
 
 tableName = "Politiekers"
 url = f'https://api.airtable.com/v0/app5SoKsYnuOY96ef/{tableName}?api_key=key2Jl1YfS4WWBFa5'
 
-params = dict(
-    origin='Chicago,IL',
-    destination='Los+Angeles,CA',
-    waypoints='Joplin,MO|Oklahoma+City,OK',
-    sensor='false'
-)
 
-resp = requests.get(url=url) # , params=params
+conn = psycopg2.connect(host="localhost",database="kiestze", user=secret.psql_user, password=secret.psql_password)
+cur = conn.cursor()
+
+resp = requests.get(url=url)
 json_object = resp.json() # Check the JSON Response Content documentation below
-#print(json_object["records"][0])
 
 
 for record in json_object["records"]:
-  #print(record)
-  for fieldKey in record["fields"]:
-    print(fieldKey)
-    print(record["fields"][fieldKey])
-    # now song is a dictionary
-    #for attribute, value in song.iteritems():
-    #    print(attribute, value) # example usage
+	naam_stukken = record["fields"]["Naam"].split(' ')
+	cur.execute("SELECT * FROM kiestze_politieker WHERE naam ILIKE '%"+naam_stukken[0]+"%' AND  naam ILIKE '%"+naam_stukken[-1]+"%';")
+	politieker_from_db = cur.fetchone()
+
+	if politieker_from_db is None:
+		print("NOT FOUND: "+record["fields"]["Naam"] + "   -- Partij: " + ", ".join(record["fields"]["Partij"]))
+		continue
+
+	politieker_id = politieker_from_db[0] # id is first element
+
+	for fieldKey in record["fields"]:
+		if fieldKey in ["Partij", "Mandaten", "Naam"]:
+			print("IGNORED")
+			continue
+
+		fieldValue = record["fields"][fieldKey]
+		if isinstance(fieldValue, list):
+			# assume this is image
+			fieldValue = fieldValue[0]["thumbnails"]["large"]["url"]
+
+		print(fieldValue)
+
+		guid = uuid.uuid4()
+		column_name = fieldKey
+		accepted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		suggested_value = fieldValue
+
+		sql += "('%s', '%s', '%s', '%s', %s),\n" % (guid, column_name, accepted_date, suggested_value, politieker_id)
+		print(" ")
+
 print(type("test"))
-sql_Partij = """
-
-DROP TABLE IF EXISTS `air_Politiekers`;
-CREATE TABLE `air_Politiekers` (
-  `id` varchar(18) NOT NULL AUTO_INCREMENT,
-  `otb_id` int(11) DEFAULT NULL,
-  `naam` varchar(50) NOT NULL,
-  `geboorte` date DEFAULT NULL,
-  `geslacht` char(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `air_Politiekers` (`id`, `otb_id`, `naam`, `geboorte`, `geslacht`) VALUES
-
-"""
 # example record key: recmbyKsOdabhIOCd
 
+sql = sql[:-2] # remove trailing comma
+sql += "\n;\n\n\n\n\n"
+
 f = open("airtables_postgresql.sql","wb")
-f.write(monster_query.encode("UTF-8"))
+f.write(sql.encode("UTF-8"))
