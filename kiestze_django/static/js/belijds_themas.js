666d61e6cd6d86f3f3e3ab638986f8f97befc0b6
Emile Sonneveld <contact@emilesonneveld.be>
2018-09-12 15:42:15 +0200

    python build_steps.py

---
+++ kiestze_django/static/js/belijds_themas.js
@@ -0,0 +1,22 @@
+// Avoid delimeter cahracters like comma, and pipe| symbol!
+const belijds_themas = [
+	"-", // niet ingevuld
+	"gezondheid",
+	"mobiliteit",
+	"bejaardenzorg",
+	"wonen",
+	"jeugd",
+	"asiel en migratie",
+	"veiligheid",
+	"milieu / energie",
+	"onderwijs",
+	"tewerkstelling",
+	"verkeer",
+	"dierenwelzijn",
+	"sport",
+	"cultuur",
+	"werk en inkomen",
+	"economie",
+]
+
+export { belijds_themas };
