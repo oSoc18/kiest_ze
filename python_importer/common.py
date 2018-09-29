aeb12119ef104a14d06ee33062962431d4a1bb3b
Emile Sonneveld <contact@emilesonneveld.be>
2018-09-29 10:41:54 +0200

    Import WIP.

---
+++ python_importer/common.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+# HACK: https://stackoverflow.com/questions/7505988/importing-from-a-relative-path-in-python/7506029#7506029
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'kiestze_django', 'kiestze_django'))
+import secret
+#import ..kiestze_django/kiestze_django/secret.py
+
+partijen_tablename = 'kiestze_partij'
+politiekers_tablename = 'kiestze_politieker'
+politieker_partijen_link_tablename = 'kiestze_politiekerpartijlink'
+
+conn = psycopg2.connect(host="localhost",database="kiestze", user=secret.psql_user, password=secret.psql_password)
---
+++ python_importer/import2018.py
@@ -0,0 +1,40 @@
+import requests
+import uuid  # https://github.com/skorokithakis/shortuuid
+import datetime
+from time import sleep
+import csv
+
+import common # must be last import
+
+# C:\Users\emill\dev\kiest_ze\python_importer\airtables_postgresql.py
+# C:\Users\emill\dev\kiest_ze\kiestze_django\kiestze_django\secret.py
+
+cur = common.conn.cursor()
+
+sql_politiekers = "INSERT INTO %s (id, otb_id, naam, geboorte, geslacht) VALUES\n" % common.politiekers_tablename
+
+cur.execute("SELECT MAX(id) FROM kiestze_politieker;")
+numer_of_politicians = cur.fetchone()
+incerementing_politieker_id = numer_of_politicians[0]
+
+
+with open('Export_ADef_TtesInfos_CGV_21092018_13h02_pers.csv', newline='', encoding="utf8") as csvfile:
+	reader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+	for row in reader:
+		print(row)
+		naam = row['Achternaam_stembiljet'] + " " + row["voornaam_stembiljet"]
+		nis = int(row['NIS'])
+
+		cur.execute("SELECT * FROM kiestze_politieker WHERE naam = '" + naam + "';")
+		politieker_from_db = cur.fetchone()
+		if politieker_from_db is None:
+			sql_politiekers += f"({incerementing_politieker_id},	0,	'{naam}',	NULL,	NULL),\n"
+
+
+sql_politiekers = sql_politiekers[:-2] # remove trailing comma
+sql_politiekers += ";\n\n\n\n\n\n"
+
+monster_query = sql_politiekers
+
+f = open("import2018.sql","wb")
+f.write(monster_query.encode("UTF-8"))
